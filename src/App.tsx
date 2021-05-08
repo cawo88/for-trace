f84ef753fe9bf3de4a9aa0d67048bab359fe07b5
Carol Leung <carol.carl.leung@gmail.com>
2021-05-08 19:20:50 +0200

    feat: add privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Error } from './pages/Error';
+import { PrivacyPolicy } from './pages/PrivacyPolicy';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/privacy-policy" component={PrivacyPolicy} />
           <Route path="/404" component={Error} />
           <Redirect to="/404" />
         </Switch>
