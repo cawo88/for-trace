81a43503481e80f7ad2c1d0c87535ca44b70f950
Carol Leung <carol.carl.leung@gmail.com>
2021-05-15 20:53:05 +0200

    feat: lazy load components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Redirect, Route, Switch, BrowserRouter } from 'react-router-dom';
-import { Home } from './pages/Home';
+// import { Home } from './pages/Home';
 import { Error } from './pages/Error';
-import { PrivacyPolicy } from './pages/PrivacyPolicy';
+
+const Home = lazy(() => import('./pages/Home').then((module) => ({ default: module.Home })));
+const PrivacyPolicy = lazy(() => import('./pages/PrivacyPolicy').then((module) => ({ default: module.PrivacyPolicy })));
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/datenschutz" component={PrivacyPolicy} />
+          <Suspense
+            fallback={
+              <article style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <h1 style={{ fontSize: '42px' }}>Laden...</h1>
+              </article>
+            }
+          >
+            <Route exact path="/" component={Home} />
+            <Route path="/datenschutz" component={PrivacyPolicy} />
+          </Suspense>
           <Route path="/404" component={Error} />
           <Redirect to="/404" />
         </Switch>
